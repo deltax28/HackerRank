56c38585792d553d7bced49b5e7103f40f1b1a2b
Deepak G <deepu.g.92@gmail.com>
2016-10-25 16:07:09 +0530

    Create CamelCase.java

---
+++ Algorith/Implementation/CamelCase.java
@@ -0,0 +1,24 @@
+/*
+* HACKER RANK -> ALGORITHM -> STRINGS
+* CamelCase Problem 
+*/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        int wordCount=1;
+        for(int i = 0 ; i < s.length(); i++){
+            if(Character.isUpperCase(s.charAt(i)))
+                wordCount++;
+        }
+        System.out.println(wordCount);
+    }
+}
