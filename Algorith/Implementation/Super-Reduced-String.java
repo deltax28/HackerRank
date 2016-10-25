e5690b7433bb8c7eb5b3add9ccdc432d60a6c89c
Deepak G <deepu.g.92@gmail.com>
2016-10-25 16:01:01 +0530

    Create Super-Reduced-String.java

---
+++ Algorith/Implementation/Super-Reduced-String.java
@@ -0,0 +1,29 @@
+/*
+* HACKER RANK -> ALGORITHM -> IMPLEMENTATION
+* Super Reduced String Problem 
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        Scanner in = new Scanner (System.in);
+        StringBuilder s = new StringBuilder(in.next());
+       
+        for(int i = 1 ; i < s.length(); i++){
+            if(s.charAt(i) == s.charAt(i-1)){
+                s.delete(i-1,i+1);
+                i=0;   
+            }
+        }
+        
+        if(s.length() == 0)
+            System.out.println("Empty String");
+        else
+            System.out.println(s);
+    }
+}
